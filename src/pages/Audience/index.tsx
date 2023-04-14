5b55fd3ee2b9ff7dfd41959644b9ba1a170ad3da
Nmgix <frostdraky@gmail.com>
2023-04-14 21:44:02 +0300

    Пофиксил страницы

---
+++ src/pages/Audience/index.tsx
@@ -0,0 +1,16 @@
+import AddObject from "../../components/AddObject";
+// import { useParams } from "react-router-dom";
+// import ViewAudience from '../../components/viewAudience'
+
+const Audience = () => {
+  // let { id } = useParams();
+
+  return (
+    <main>
+      {/* <ViewAudience/> */}
+      <AddObject />
+    </main>
+  );
+};
+
+export default Audience;
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,18 @@
+import AddAudience from "../../components/AddAudience";
+import Search from "../../components/Search";
+import ViewAudience from "../../components/viewAudience";
+import styles from "./main.module.scss";
+
+const Main = () => {
+  return (
+    <main className={styles.wrapperMain}>
+      <ViewAudience />
+      <div>
+        <Search />
+        <AddAudience />
+      </div>
+    </main>
+  );
+};
+
+export default Main;
