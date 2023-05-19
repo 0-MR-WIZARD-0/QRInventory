7239d11c4650502a0f63a7ab8275fdf872ee3a20
Boris Zunnunov <boriszunun74@gmail.com>
2023-05-20 00:46:55 +0300

    fix error

--- src/pages/Views/Main/Cabinets/Scenario/index.tsx
+++ src/pages/Views/Main/Cabinets/Scenario/index.tsx
@@ -23,38 +23,10 @@ export const CreateCabinetScenarioComponent: React.FC = () => {
 
   const [cabinetNumber, setCabinetNumber] = useState<string>("");
 
-
-  // useEffect(() => {
-  //   setInstitution(valueSelector);
-  // }, [valueSelector]);
-
-  const createCabinet = (value: string) => {
-    // (async () => {
-    //   try {
-    //     let res = await api.post("/cabinet/create", { institution: institution.id, cabinetNumber: value });
-    //     if (res.status === 200) {
-    //       await api.get("/cabinet/all").then(res => {
-    //         updateCabinets(res.data);
-    //       });
-
-    //     } else {
-    //       // console.log(res.data);
-    //       // setError("Произошла ошибка при создании кабинета");
-    //     }
-    //     }
-    //   } catch (error) {
-    //     // console.log(error);
-    //     // setError("Произошла ошибка при создании кабинета");
-    //   }
-    // })();
-  };
-
   return (
     <div className={styles.createCabinet}>
       <h2>Создание кабинета</h2>
-      {/* <input onChange={e => setCabinetNumber(e.target.value)} placeholder='Номер кабинета' /> */}
       <Input name='cabinet-number' value={cabinetNumber} onChange={e => setCabinetNumber(e.target.value)} placeholder={"503-А"} label='Номер кабинета' />
-      {/* <button onClick={() => createCabinet(cabinetNumber)}>Создать</button> */}
       <DefaultButton component={<>Создать</>} onSumbit={() => createCabinet(cabinetNumber)} />
     </div>
   );
--- src/pages/Views/Main/Items/Scenario/index.tsx
+++ src/pages/Views/Main/Items/Scenario/index.tsx
@@ -12,7 +12,6 @@ const CreateItemScenarioComponent: React.FC = () => {
   const [article, setArticle] = useState<string>("");
   const [name, setName] = useState<string>("");
 
-<<<<<<< HEAD
   const createItem = (article: string, name: string) => {
     (async () => {
       try {
@@ -32,45 +31,9 @@ const CreateItemScenarioComponent: React.FC = () => {
         console.log(error);
       }
     })();
-=======
-  // const createItem = (article: string, name: string) => {
-  //   (async () => {
-  //     try {
-  //       let res = await api.post("/auth/register", {
-  //         fullName: article.toString(),
-  //         email: name.toString(),
-  //       });
-  //       if (res.status === 200) {
-  //         // createUsers(res.data);
-  //         console.log(res.data);
-  //       } else {
-  //         console.log(res.data);
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   })();
-  // }
-
-  return (
-    <div>
-      <h2>Создание предмета</h2>
-      <input placeholder='Артикул' onChange={e => setArticle(e.target.value)} />
-      <input placeholder='Название предмета' onChange={e => setName(e.target.value)} />
-      {/* <button onClick={(e)=>{createItem(article, name)}}>Создать</button> */}
-    </div>
-  );
-};
-
-export const CreateItemScript: Script = {
-  0: {
-    content: CreateItemScenarioComponent,
-    onFailure: -1,
-    onSuccess: -1
->>>>>>> bd7869fa74d409cb583b36deeb69c47cfdadc9ba
   }
 
-};
+
 
 
     return (
@@ -92,7 +55,7 @@ export const CreateItemScript: Script = {
         <DefaultButton component={<>Создать</>} onSumbit={() => {}} />
       </div>
     );
-  };
+  }
 
   export const CreateItemScript: Script = {
     0: {
