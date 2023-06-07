0e398a84d773a7866c9cd5d284ce6906ff7150ce
Nmgix <frostdraky@gmail.com>
2023-06-07 03:31:38 +0300

    Немного починил loader

--- src/components/Complex/Wrappers/ViewsWrapper/index.tsx
+++ src/components/Complex/Wrappers/ViewsWrapper/index.tsx
@@ -1,4 +1,5 @@
 // import { LoadingTransitionComponent } from "components/Basic/Loader";
+import { LoadingTransitionComponent } from "components/Basic/Loader";
 import ProtectedComponent from "components/Protected/Component";
 import { Roles } from "types/User";
 import styles from "./view.wrapper.module.scss";
@@ -10,27 +11,24 @@ type ViewsWrapperProps = {
   error: string | undefined;
 };
 const ViewsWrapper: React.FC<ViewsWrapperProps> = ({ children, addNewButton, loading, error }) => {
-
   return (
     <div className={styles.wrapper}>
       {addNewButton}
-      {children !== undefined && children.length > 0 ? (
-        children
-      ) : (
-        <div className={styles.noDataBoundary}>
-          <h4>Нет данных</h4>
-          <ProtectedComponent component={<p>проверьте выбранное учреждение</p>} roles={[Roles.admin]} />
-        </div>
-      )}
+      {children !== undefined && loading === false && children.length > 0
+        ? children
+        : loading === false && (
+            <div className={styles.noDataBoundary}>
+              <h4>Нет данных</h4>
+              <ProtectedComponent component={<p>проверьте выбранное учреждение</p>} roles={[Roles.admin]} />
+            </div>
+          )}
       {error ? (
         <div className={styles.noDataBoundary}>
           <h4>Произошла ошибка при загрузке данных</h4>
         </div>
       ) : (
-        // loading === true && <LoadingTransitionComponent />
-        ""
-      )
-      }
+        loading === true && <LoadingTransitionComponent />
+      )}
     </div>
   );
 };
